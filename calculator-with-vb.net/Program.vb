e688e9850ce6441de93255afdc95c2042b30b525
ashori2002 <mohammadashori2002@gmail.com>
2023-08-21 23:26:35 +0330

    End

--- calculator-with-vb.net/Program.vb
+++ calculator-with-vb.net/Program.vb
@@ -30,3 +30,44 @@ Public Class Calculator
         End If
     End Function
 End Class
+
+Module Program
+    Sub Main()
+        Dim calculator As New Calculator()
+
+        Console.WriteLine("Enter the first number:")
+        Dim num1 As Double = Double.Parse(Console.ReadLine())
+
+        Console.WriteLine("Enter the second number:")
+        Dim num2 As Double = Double.Parse(Console.ReadLine())
+
+        Console.WriteLine("Select an operation:")
+        Console.WriteLine("1. Addition")
+        Console.WriteLine("2. Subtraction")
+        Console.WriteLine("3. Multiplication")
+        Console.WriteLine("4. Division")
+        Console.WriteLine("Enter the operation number:")
+        Dim operation As Integer = Integer.Parse(Console.ReadLine())
+
+        Dim result As Double
+
+        Select Case operation
+            Case 1
+                result = calculator.Add(num1, num2)
+                Console.WriteLine("Result: " & result)
+            Case 2
+                result = calculator.Subtract(num1, num2)
+                Console.WriteLine("Result: " & result)
+            Case 3
+                result = calculator.Multiply(num1, num2)
+                Console.WriteLine("Result: " & result)
+            Case 4
+                result = calculator.Divide(num1, num2)
+                If Not Double.IsNaN(result) Then
+                    Console.WriteLine("Result: " & result)
+                End If
+            Case Else
+                Console.WriteLine("Invalid operation selected.")
+        End Select
+    End Sub
+End Module
