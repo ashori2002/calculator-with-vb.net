5ac1460bc1be0ea6e719ea88ad65df312026bace
ashori2002 <mohammadashori2002@gmail.com>
2023-08-21 23:23:26 +0330

    Subtracts num2 from num1

--- calculator-with-vb.net/Program.vb
+++ calculator-with-vb.net/Program.vb
@@ -1,8 +1,11 @@
 Imports System
 
-Module Program
-    Sub Main(args As String())
-        Console.WriteLine("Hello World!")
-
+Public Class Calculator
+    Public Sub New()
     End Sub
-End Module
+
+    Public Function Add(ByVal num1 As Double, ByVal num2 As Double) As Double
+        ' Adds two numbers
+        Return num1 + num2
+    End Function
+
